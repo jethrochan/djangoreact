ebf9cb1773f0834434d8eb5ca309ad952d3f4e50
Jethro Chan <jethro@jethrochan.com>
2020-02-28 08:22:37 -0800

    Clean up comments. Remove unused import.

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-#we whitelist localhost:3000 because that's where frontend will be served
+# whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
